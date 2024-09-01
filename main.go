c82d323ea777697156b6117f0431e9e17e21a9b4
RobertGee <robertgouveia03@hotmail.com>
2024-09-01 13:01:17 +0100

    Changed main

--- main.go
+++ main.go
@@ -9,16 +9,14 @@ var tmpl = template.Must(template.ParseFiles("index.html"))
 
 //panic if template is not found
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"Message": "Hello, World!",
-	}
+func render(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{}
 
 	tmpl.Execute(w, data)
 	// you can pass data to the template by passing a map
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/", render)
 	http.ListenAndServe(":8080", nil)
 }
